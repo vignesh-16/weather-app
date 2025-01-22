835d3493007af83fdb5f8688c91c0d9c7fbf104e
vignesh-16 <assassincreedlll07@gmail.com>
2025-01-22 19:41:01 +0530

    Added logic to extract jwt token claims in order to validate it using jwt filter.

--- backend/src/main/java/com/vignesh/weather/services/JwtService.java
+++ backend/src/main/java/com/vignesh/weather/services/JwtService.java
@@ -1,8 +1,10 @@
 package com.vignesh.weather.services;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.KeyGenerator;
@@ -11,6 +13,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -44,5 +47,36 @@ public class JwtService {
         byte[] keyInBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyInBytes);
     }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String userEmail = extractUserEmail(token);
+        return (userEmail.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiry(token).before(new Date());
+    }
+
+    private Date extractExpiry(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private String extractUserEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims,T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
 
 }
