7cee3794b4edb40ead9dc155fb28594b3807ec22
vignesh-16 <assassincreedlll07@gmail.com>
2025-02-06 19:04:53 +0530

    [Front-end]:: Input of user credentials will be bordered by red if user does not provide proper data.

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,13 +1,32 @@
+import { useState } from "react";
+
 const Login = ()=> {
+    const [ userid, setUserId ] = useState(null);
+    const [ password, setPassword ] = useState(null);
+
+    const login = ()=>{
+        if((userid !== null && userid !== '') 
+            && (password !== null && password !== '')) 
+        {
+            console.info(`Here are the credentials: userid: ${userid}, password: ${password}`);
+        } else {
+            if (userid === null) {
+                setUserId('invalid');
+            }
+            if (password === null) {
+                setPassword('invalid');
+            }
+        }
+    }
     return (
         <>
             <div className="login-section-container">
                 <div className="user-credentials-section">
-                    <input type="text"  name="userid" id="userid" placeholder="Enter email or username"></input>
-                    <input type="password" name="password" id="password" placeholder="Enter password"></input>
+                    <input type="text" className={`${userid === 'invalid' ? 'is-invalid' : ''}`} name="userid" id="userid" onChange={ (e)=>{ setUserId(e?.currentTarget?.value) } } placeholder="Enter email or username"></input>
+                    <input type="password" className={`${password === 'invalid' ? 'is-invalid' : ''}`} name="password" id="password" onChange={ (e)=>{ setPassword(e?.currentTarget?.value) } } placeholder="Enter password"></input>
                 </div>
                 <div className="verify-action-section">
-                    <button className="login-click">login</button>
+                    <button className="login-click" onClick={ (e)=>{ e.preventDefault(); login() } }>login</button>
                     <button className="forgot-password">Forgot Password?</button>
                 </div>
                 <div className="create-an-account">
