6d3880637d28dd454497a5b769cf85e72d491bcb
vignesh-16 <assassincreedlll07@gmail.com>
2025-02-09 10:31:16 +0530

    [front-end]:: Added a new favicon for the web application. -> ProctectedRoute enabled for all routes excluding login route.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './components/Home';
 import SignUp from './components/SignUp';
 import ResetPassword from './components/ResetPassword';
 import Account from './components/Account';
+
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem("authToken");
+  return token ? children : <Navigate to="/" />;
+};
 
 function App() {
   return (
@@ -13,16 +18,16 @@ function App() {
         <Route path='/' element={<Login />} />
       </Routes>
       <Routes>
-        <Route path='/home' element={<Home />} />
+        <Route path='/home' element={<ProtectedRoute><Home /></ProtectedRoute>} />
       </Routes>
       <Routes>
-        <Route path='/signup' element={<SignUp />} />
+        <Route path='/signup' element={<ProtectedRoute><SignUp /></ProtectedRoute>} />
       </Routes>
       <Routes>
-        <Route path='/forgotpassword' element={<ResetPassword />} />
+        <Route path='/forgotpassword' element={<ProtectedRoute><ResetPassword /></ProtectedRoute>} />
       </Routes>
       <Routes>
-        <Route path='/myAccount' element={<Account />} />
+        <Route path='/myAccount' element={<ProtectedRoute><Account /></ProtectedRoute>} />
       </Routes>
     </Router> 
   );
