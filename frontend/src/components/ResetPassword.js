a4f91ab691599cdc27264040f658f94d40495134
vignesh-16 <assassincreedlll07@gmail.com>
2025-02-27 19:02:02 +0530

    [Front-end]:: Wrapping-up login pages - simple input and states for reset password section.

--- frontend/src/components/ResetPassword.js
+++ frontend/src/components/ResetPassword.js
@@ -1,8 +1,29 @@
+import { useState } from 'react';
+
 const ResetPassword = ()=> {
+
+    const [userEmail, setUserEmail] = useState(null);
+    const [showText, setShowText] = useState("Send verification code");
+    const [thisStep, setThisStep] = useState('isUserExists')
+
+    const nextClick = async()=>{
+        if(thisStep === 'isUserExists') {
+            console.log('Make a back-end call to check if user exists with provided email!');
+            setShowText('Confirm verification code');
+        } else {
+            console.log('Verify the inserted verification code is valid or not');
+        }
+    }
+
     return (
         <>
             <div className="reset-password-section-container">
-                <span>Reset Password bro!</span>
+                <div className="user-credentials-section">
+                    <input type="text" className={`${userEmail === 'invalid' ? 'is-invalid' : ''}`} name="userid" id="userid" onChange={ (e)=>{ setUserEmail(e?.currentTarget?.value) } } placeholder="Enter your email"></input>
+                </div>
+                <div className='next-step-button-holder'>
+                    <button className={`next-step ${thisStep}`} id='next' onClick={ (e)=>{ e.preventDefault(); nextClick() } } >{showText}</button>
+                </div>
             </div>
         </>
     )
