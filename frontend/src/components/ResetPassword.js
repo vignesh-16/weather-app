64a2aa820a4d1cf2346092399b8b5f5e9b42b954
vignesh-16 <assassincreedlll07@gmail.com>
2025-03-01 18:50:52 +0530

    [Front-end]:: Few UI changes for reset password page.

--- frontend/src/components/ResetPassword.js
+++ frontend/src/components/ResetPassword.js
@@ -5,16 +5,19 @@ import useGet from '../hooks/useGet'
 const ResetPassword = ()=> {
 
     const [userEmail, setUserEmail] = useState(null);
-    const [showText, setShowText] = useState("Send verification code");
+    const [showText, setShowText] = useState("Confirm Email");
     const [thisStep, setThisStep] = useState('isUserExists')
     const getUserDetails = useGet(ENDPOINT.GET_USER_INFO);
 
     const nextClick = async()=>{
+        document.getElementsByClassName('load-screen-reset-page')[0].classList.add('loader');
+        document.getElementsByClassName('next-step')[0].classList.add('button-blue');
         if(thisStep === 'isUserExists' && userEmail !== null) {
             let response = await getUserDetails(userEmail);
             console.log('[RESET][INFO] Response from server: ',response);
-            setShowText('Confirm verification code');
+            setShowText('Send verification code');
         } else {
+            setShowText('Confirm verification code');
             console.log('Verify the inserted verification code is valid or not');
         }
     }
@@ -26,7 +29,10 @@ const ResetPassword = ()=> {
                     <input type="text" className={`${userEmail === 'invalid' ? 'is-invalid' : ''}`} name="userid" id="userid" onChange={ (e)=>{ setUserEmail(e?.currentTarget?.value) } } placeholder="Enter your email"></input>
                 </div>
                 <div className='next-step-button-holder'>
-                    <button className={`next-step ${thisStep}`} id='next' onClick={ (e)=>{ e.preventDefault(); nextClick() } } >{showText}</button>
+                    <button className={`button next-step ${thisStep}`} id='next' onClick={ (e)=>{ e.preventDefault(); nextClick() } } >
+                        <span className="load-screen-reset-page"></span>
+                        <span className='button-label'>{showText}</span>
+                    </button>
                 </div>
             </div>
         </>
